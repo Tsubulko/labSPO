11cc2d3a97667eedb61beee85084b3a0c0fd27a0
Tsubulko <90478149+Tsubulko@users.noreply.github.com>
2023-04-26 21:28:08 +0300

    Upd lab6

--- lab6.java
+++ lab6.java
@@ -12,19 +12,29 @@ public class Main {
     private static Semaphore semaphore = new Semaphore(SEMAPHORE_PERMITS);
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(() -> {
-            Random random = new Random();
-            for (int i = 0; i < ARRAY_SIZE; i++) {
-                int number = random.nextInt(MAX_VALUE + 1);
-                array[i] = number;
-                System.out.println("Поток 1: " + number);
-                try {
-                    Thread.sleep(500); 
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+   
+            Thread thread1 = new Thread(() -> {
+    try {
+        semaphore.acquire(); 
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+    Random random = new Random();
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        int number = random.nextInt(MAX_VALUE + 1);
+        array[i] = number;
+        System.out.println("Поток 1: " + number);
+        try {
+            Thread.sleep(500); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    semaphore.release(); 
+});
+
 
         Thread thread2 = new Thread(() -> {
             try {
